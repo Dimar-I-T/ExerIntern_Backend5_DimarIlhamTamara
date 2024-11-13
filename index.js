b5b5c9467f383c0cbadef050e2692507b4c8d8d6
Dimar Ilham Tamara <dimartamara1@gmail.com>
2024-11-13 07:32:27 +0700

    1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+const user_router = require('./routes/user_routes');
+const chat_router = require('./routes/chat_routes');
+require("./mongo.config").connectDB();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.use('/user', user_router);
+app.use('/chat', chat_router);
+
+app.listen(port);
